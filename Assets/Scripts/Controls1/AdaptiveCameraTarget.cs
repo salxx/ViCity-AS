0e2ffa7ee48562a112ff2e400e5a3d0b7b1a1d07
Alex <alexander.sartory@gmail.com>
2021-09-28 08:52:26 +0200

    camera impl improved

--- Assets/Scripts/Controls1/AdaptiveCameraTarget.cs
+++ Assets/Scripts/Controls1/AdaptiveCameraTarget.cs
@@ -6,6 +6,9 @@ public class AdaptiveCameraTarget : MonoBehaviour
 {
     public static List<AdaptiveCameraTarget> cameraTargets = new List<AdaptiveCameraTarget>();
 
+    public bool isExhibit = false;
+    public float exhibitFocusDistance = 3f;
+
     private void OnEnable()
     {
         cameraTargets.Add(this);
--- Assets/Scripts/Controls1/CameraFollowPlayerScriptNet.cs
+++ Assets/Scripts/Controls1/CameraFollowPlayerScriptNet.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class CameraFollowPlayerScriptNet : MonoBehaviour
 {
     private Transform player;
+    static float CAMERA_SMOOTH = 5f;
 
     private void LateUpdate()
     {
@@ -25,23 +26,32 @@ public class CameraFollowPlayerScriptNet : MonoBehaviour
     {
         string log = "";
         Vector3 newDestination;
-        float MIN_DISTANCE = 2f;
         float MAX_DISTANCE = 7f;
-        float DELTA_DISTANCE = MAX_DISTANCE - MIN_DISTANCE;
+
+        // first check if player is close to exhibit
+        AdaptiveCameraTarget closeExhibit;
+        if (IsInExhibitFocusRange(out closeExhibit))
+        {
+            Debug.Log("Focus on exhibit: " + closeExhibit.name);
+            newDestination = closeExhibit.transform.position;
+            newDestination.y = player.position.y;
+            transform.position = Vector3.Lerp(transform.position, newDestination, Time.deltaTime * CAMERA_SMOOTH);
+            return;
+        }
+
+        // second: find and interpolate between all close targets
         List<AdaptiveCameraTarget> targetsInRange = new List<AdaptiveCameraTarget>();
         targetsInRange.AddRange(AdaptiveCameraTarget.cameraTargets);
-        targetsInRange.RemoveAll(t => (t.transform.position - player.position).magnitude > MAX_DISTANCE);
+        targetsInRange.RemoveAll(t => CalculateTargetDistanceFromPlayer(t) > MAX_DISTANCE);
         targetsInRange.Remove(player.GetComponent<AdaptiveCameraTarget>());
         if (targetsInRange.Count > 0)
         {
-            //transform.position = Vector3.Lerp(targetsInRange[0].transform.position, player.position, (distance - MIN_DISTANCE) / DELTA_DISTANCE);
             Vector3 center = Vector3.zero;
             float maxWeight = 0f;
             float w;
             targetsInRange.ForEach(t => {
-                float distance = (t.transform.position - player.position).magnitude;
-                distance -= MIN_DISTANCE;
-                w = (DELTA_DISTANCE - distance) / DELTA_DISTANCE;
+                float distance = CalculateTargetDistanceFromPlayer(t);
+                w = Mathf.Pow((MAX_DISTANCE - distance) / MAX_DISTANCE, 2f);
                 log += " w: " + w;
                 maxWeight += w;
                 center += t.transform.position * w;
@@ -55,16 +65,53 @@ public class CameraFollowPlayerScriptNet : MonoBehaviour
         {
             newDestination = player.position;
         }
-        if((transform.position - newDestination).magnitude > 0.05f) {
-            transform.position = Vector3.Lerp(transform.position, newDestination, Time.deltaTime * 15f);
+
+        newDestination.y = player.position.y;
+        if ((transform.position - newDestination).magnitude > 0.05f) {
+            transform.position = Vector3.Lerp(transform.position, newDestination, Time.deltaTime * CAMERA_SMOOTH);
         }
+
         targetsInRange.ForEach(t => {
-            float distance = (t.transform.position - player.position).magnitude;
-            distance -= MIN_DISTANCE;
-            float w = (DELTA_DISTANCE - distance) / DELTA_DISTANCE;
+            float distance = CalculateTargetDistanceFromPlayer(t);
+            float w = Mathf.Pow((MAX_DISTANCE - distance) / MAX_DISTANCE, 2f);
             Debug.DrawLine(t.transform.position + Vector3.up, transform.position + Vector3.up, Color.green * w);
         });
-        Debug.DrawLine(player.position + Vector3.up, transform.position + Vector3.up, Color.blue);
+        //Debug.DrawLine(player.position + Vector3.up, transform.position + Vector3.up, Color.blue);
         Debug.Log(log);
+    }
+
+    private float CalculateTargetDistanceFromPlayer(AdaptiveCameraTarget t)
+    {
+        Vector3 from = t.transform.position;
+        from.y = player.position.y;
+        return (from - player.position).magnitude;
+    }
+
+    private bool IsInExhibitFocusRange(out AdaptiveCameraTarget exhibit)
+    {
+        List<AdaptiveCameraTarget> targetsInRange = new List<AdaptiveCameraTarget>();
+        targetsInRange.AddRange(AdaptiveCameraTarget.cameraTargets);
+        targetsInRange.Remove(player.GetComponent<AdaptiveCameraTarget>());
+        targetsInRange.RemoveAll(t => !t.isExhibit || CalculateTargetDistanceFromPlayer(t) > t.exhibitFocusDistance);
+        if(targetsInRange.Count == 0)
+        {
+            exhibit = null;
+            return false;
+        } else if(targetsInRange.Count == 1)
+        {
+            exhibit = targetsInRange[0];
+            return true;
+        } else // find closest exhibit
+        {
+            exhibit = targetsInRange[0];
+            foreach (AdaptiveCameraTarget t in targetsInRange)
+            {
+                if(t.exhibitFocusDistance < exhibit.exhibitFocusDistance)
+                {
+                    exhibit = t;
+                }
+            }
+            return true;
+        }
     }
 }
